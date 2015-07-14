ad67610cbc2958c8037fcf6c30ae704e89fde502
hasfuat <fuat@tune.com>
2015-07-14 15:58:59 -0700

    Update readere.java
    done some redundancy, commented and added function for runnable jar

--- com/tune/play/readere.java
+++ com/tune/play/readere.java
@@ -1,5 +1,6 @@
 package play;
 
+
 import java.awt.List;
 import java.beans.Statement;
 import java.io.BufferedReader;
@@ -43,88 +44,105 @@ import org.json.simple.parser.ParseException;
 
 import scala.tools.cmd.Parser;
 import akka.pattern.Patterns;
-import MyGame.rawLog;
 
-public class readere {
+import com.tune.MyGame.rawLog;
+/**
+ * Takes dates from json file, and search for matches.
+ * @author fuat, devang
+ *
+ */
+public class Redere {
 
 	private static String driverName = "org.apache.hive.jdbc.HiveDriver";
 	static JSONObject jsonArray;
-	public static void main(String[] args) throws SQLException, IOException, ClassNotFoundException, JSONException
-	{
+
+	/**
+	 * Main program
+	 * 
+	 * @param args the file names.
+	 * @throws SQLException sql exception.
+	 * @throws IOException io exception.
+	 * @throws ClassNotFoundException class not found.
+	 * @throws JSONException json exception.
+	 */
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) throws SQLException, IOException,
+			ClassNotFoundException, JSONException {
+
+		if (args.length == 0) {
+			System.out
+					.println("To run this jar: java -jar <jarname> <JSON local file path>  <~/hadoop/etc/hadoop/core-site.xml>");
+			System.exit(0);
+		}
+
 		System.out.println("Starting the parser");
 		ArrayList<String> files = new ArrayList<String>();
 		ArrayList<String> folders = new ArrayList<String>();
 		ArrayList<String> patterns = new ArrayList<String>();
-		
-		//Properties pro = Utility.readParameters("/home/devang/Desktop/IMP/property.properties");
+		ArrayList<Integer> advertiser_id = new ArrayList<Integer>();
+		ArrayList<Integer> prison_id = new ArrayList<Integer>();
+
 		JSONParser parser = new JSONParser();
 		try {
-			//Object obj = parser.parse(new FileReader("/home/devang/Desktop/IMP/property.json"));
-			Object obj = parser.parse(new FileReader("/home/hadoop/devang/property.json"));
+
+			Object obj = parser.parse(new FileReader(args[0]));
 			jsonArray = (JSONObject) obj;
-			
+
 		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+
+			System.err.println("Cannot parse to json object! " + e);
 		}
-		
-		ArrayList<Integer> advertiser_id = new ArrayList<Integer>();
+
 		JSONArray ad_Id = (JSONArray) jsonArray.get("Advertiser_ID");
-		for (int i =0 ; i<ad_Id.size() ; i++)
-		{
-			//int v= Integer.parseInt(advertisers[i]);
-			//System.out.println(Integer.parseInt(advertisers[i]));
+		for (int i = 0; i < ad_Id.size(); i++) {
 			advertiser_id.add(Integer.parseInt((String) ad_Id.get(i)));
 		}
-		String[] min =((String) jsonArray.get("Min")).split(" ");
-		String[] max =((String) jsonArray.get("Max")).split(" ");
+		String[] min = ((String) jsonArray.get("Min")).split(" ");
+		String[] max = ((String) jsonArray.get("Max")).split(" ");
 		String[] mindate = min[0].split("-");
 		String[] maxdate = max[0].split("-");
 		String[] mintime = min[1].split(":");
 		String[] maxtime = max[1].split(":");
-		System.out.println("min"+ min[0]);
-		Date startdate = new Date(Integer.parseInt(mindate[0]), Integer.parseInt(mindate[1])-1,Integer.parseInt( mindate[2]));
-		Date enddate= new Date(Integer.parseInt(maxdate[0]), Integer.parseInt(maxdate[1])-1,Integer.parseInt( maxdate[2]));
-		Time starttime = new Time(Integer.parseInt(mintime[0]), Integer.parseInt(mintime[1]), Integer.parseInt(mintime[2]));
-		Time endtime = new Time(Integer.parseInt(maxtime[0]), Integer.parseInt(maxtime[1]), Integer.parseInt(maxtime[2]));
-		
-		ArrayList<Integer> prison_id = new ArrayList<Integer>();
+		System.out.println("min" + min[0]);
+		Date startdate = new Date(Integer.parseInt(mindate[0]),
+				Integer.parseInt(mindate[1]) - 1, Integer.parseInt(mindate[2]));
+		Date enddate = new Date(Integer.parseInt(maxdate[0]),
+				Integer.parseInt(maxdate[1]) - 1, Integer.parseInt(maxdate[2]));
+		Time starttime = new Time(Integer.parseInt(mintime[0]),
+				Integer.parseInt(mintime[1]), Integer.parseInt(mintime[2]));
+		Time endtime = new Time(Integer.parseInt(maxtime[0]),
+				Integer.parseInt(maxtime[1]), Integer.parseInt(maxtime[2]));
+
 		JSONArray p_id = (JSONArray) jsonArray.get("Prison_ID");
-		for (int i =0 ; i<p_id.size() ; i++)
-		{
-			//int v= Integer.parseInt(advertisers[i]);
-			//System.out.println(Integer.parseInt(advertisers[i]));
+		
+		for (int i = 0; i < p_id.size(); i++) {
 			prison_id.add(Integer.parseInt((String) p_id.get(i)));
 		}
-		
+
 		ArrayList<Integer> producer_id = new ArrayList<Integer>();
 		JSONArray pro_id = (JSONArray) jsonArray.get("Producer_ID");
-		for (int i =0 ; i<pro_id.size() ; i++)
-		{
-			//int v= Integer.parseInt(advertisers[i]);
-			//System.out.println(Integer.parseInt(advertisers[i]));
+		for (int i = 0; i < pro_id.size(); i++) {
 			producer_id.add(Integer.parseInt((String) pro_id.get(i)));
 		}
-		
+
 		String revision_id = (String) jsonArray.get("Revision_ID");
-		
-	
-		String sequence_num= (String) jsonArray.get("Sequence_num");
-		
-		Configuration conf= new Configuration();
-		conf.addResource(new Path("/home/devang/Documents/hadoop/etc/hadoop/core-site.xml"));
+
+		String sequence_num = (String) jsonArray.get("Sequence_num");
+
+		Configuration conf = new Configuration();
+		conf.addResource(new Path(args[1]));
 		FileSystem fs1 = FileSystem.get(conf);
-		
+
 		Utility.getfoldernames(folders, startdate, enddate);
-		
-		Utility.getpatterns(patterns, startdate, enddate, revision_id, producer_id, sequence_num,
-				advertiser_id, prison_id);
-		
+
+		Utility.getpatterns(patterns, startdate, enddate, revision_id,
+				producer_id, sequence_num, advertiser_id, prison_id);
+
 		Utility.getfiles(files, folders, patterns, fs1);
-		
+
 		Utility.getdata(files, starttime, endtime, conf, driverName);
 	}
 
 }
-		
+
 		
